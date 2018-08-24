3cd18ae28ea390cc5bac9c1a820c45f9b7ed2f5f
linxiaodan <y1340787373@qq.com>
2018-08-24 18:01:12 +0800

    first commit

---
+++ dbscript/first.sql
@@ -0,0 +1,12 @@
+drop table if exists `paper`;
+create table `paper`(
+  `paper_id` bigint(20) not null auto_increment comment 'paperID',
+  `name` varchar(100) not null comment 'paper名称',
+  `pnumber` int(10) not null comment 'paper数量',
+  `detail` varchar(200) not null comment 'paper描述',
+  primary key (`paper_id`)
+)engine=InnoDB auto_increment=12 default charset=utf8 comment'paper表';
+
+insert into `paper` values ('1', '机器学习', '2', 'euwownbsjdi'),
+	('2', '深度学习', '4', 'eeeeeeeee'),
+	('3', '大数据', '10', 'riorrr');
